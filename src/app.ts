f36ff6aa91387bf78bd70cad52f0500969c3e067
Munna <mayinuddin.munna@gmail.com>
2025-05-04 16:35:46 +0600

    init

--- src/app.ts
+++ src/app.ts
@@ -1,74 +1,38 @@
-import express, { Application, Request, Response } from "express";
-import path from "path";
-import { LogsRoutes } from "./app/src/modules/Logs/logs.routes";
-import { errorlogger } from "./app/src/shared/logger";
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import express, { Application, NextFunction, Request, Response } from "express";
 
 const app: Application = express();
 
-// Serve static files like CSS
-app.use(express.static(path.join(__dirname, "../public"))); // Adjusted path
-
-// Parsers
+//parsers
 app.use(express.json());
 
-// Welcome route
-app.get("/", (req: Request, res: Response) => {
-  res.status(200).send(`
-   <html>
-      <head>
-        <title>Docker Logs Viewer</title>
-        <link rel="stylesheet" href="/styles.css">
-      </head>
-      <body>
-        <h1>Welcome to the Docker Logs Viewer Page!</h1>
-        <p>Go to <a href="/logs/errors">Error Logs</a> or <a href="/logs/successes">Success Logs</a>.</p>
-      </body>
-    </html>
-  `);
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Welcome to the Docker!!! ",
+  });
 });
 
-app.get("/error", (req: Request, res: Response) => {
-  throw new Error("This is a forced error!");
+//throwing an error
+app.get("/error", (req, res) => {
+  throw new Error("This is a forced error");
 });
 
-//Logger Routes
-app.use("/logs", LogsRoutes);
-
-// Error handler
-app.use((err: Error, req: Request, res: Response, next: any) => {
-  console.error(err);
-  errorlogger.error(err);
-
-  res.status(500).send(`
-    <html>
-      <head>
-        <title>Error</title>
-        <link rel="stylesheet" href="/styles.css">
-      </head>
-      <body>
-        <h1>Something went wrong</h1>
-        <p>${err.message}</p>
-        <a href="/">Back to Home</a>
-      </body>
-    </html>
-  `);
+//error handling
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  console.log(err);
+  res.status(500).json({
+    message: err.message,
+  });
 });
 
-// Not Found handler
+//Not Found
 app.use((req: Request, res: Response) => {
-  res.status(404).send(`
-    <html>
-      <head>
-        <title>Page Not Found</title>
-        <link rel="stylesheet" href="/styles.css">
-      </head>
-      <body>
-        <h1>Page Not Found</h1>
-        <p>The page you are looking for does not exist.</p>
-        <a href="/">Back to Home</a>
-      </body>
-    </html>
-  `);
+  res.status(404).json({
+    message: "Not Found",
+  });
 });
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,15 @@
 import { Server } from "http";
 import app from "./app";
-import { errorlogger, logger } from "./app/src/shared/logger";
 
 let server: Server;
 
 async function main() {
   try {
-    server = app.listen(process.env.PORT, () => {
-      console.log(`app is listening on port ${process.env.PORT}`);
-      logger.info(`app is listening on port ${process.env.PORT}`);
+    server = app.listen(5000, () => {
+      console.log(`app is listening on port 5000`);
     });
   } catch (err) {
     console.log(err);
-    errorlogger.error(err);
   }
 }
 
@@ -20,7 +17,6 @@ main();
 
 process.on("unhandledRejection", (err) => {
   console.log(`😈 unahandledRejection is detected , shutting down ...`, err);
-  errorlogger.error(err);
   if (server) {
     server.close(() => {
       process.exit(1);
@@ -31,6 +27,5 @@ process.on("unhandledRejection", (err) => {
 
 process.on("uncaughtException", () => {
   console.log(`😈 uncaughtException is detected , shutting down ...`);
-  errorlogger.error("uncaughtException is detected");
   process.exit(1);
 });
